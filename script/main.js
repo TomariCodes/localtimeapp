ca3900489785b904874f442f07be14d85f3dd21d
Tomari <tomari7878@gmail.com>
2026-06-24 17:41:31 -0500

    Connected fake data from javascript to HTML

--- script/main.js
+++ script/main.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+let losAngelesElement = document.getElementById("los-angeles");
+let londonElement = document.getElementById("london");
+
+let losAngelesDateElement = losAngelesElement.querySelector(".date");
+let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+
+let londonDateElement = londonElement.querySelector(".date");
+let londonTimeElement = londonElement.querySelector(".time");
+
+losAngelesDateElement.innerHTML = "June 23rd 2026";
+losAngelesTimeElement.innerHTML = `7:40:15 <span class="meridiem">PM</span>`;
+
+londonDateElement.innerHTML = "June 23rd 2026";
+londonTimeElement.innerHTML = `11:40:15 <span class="meridiem">PM</span>`;
