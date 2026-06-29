92fc01396cc7b331e85e1c4eb0702b60f5d36037
Tomari <tomari7878@gmail.com>
2026-06-29 13:43:38 -0500

    intergrated moment.js and moment timezone libraries

--- script/main.js
+++ script/main.js
@@ -1,14 +1,23 @@
 let losAngelesElement = document.getElementById("los-angeles");
 let londonElement = document.getElementById("london");
+function updateTime() {
+    // LOS ANGELES
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+    losAngelesDateElement.innerHTML = moment()
+    .tz("America/Los_Angeles")
+    .format("MMMM Do YYYY");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
+    losAngelesTimeElement.innerHTML = losAngelesTime.format("h:mm:ss [<span class=\"meridiem\">]A[</span>]");
+    
+    // LONDON
+    let londonTimeElement = londonElement.querySelector(".time");
+    let londonDateElement = londonElement.querySelector(".date");
+    
+    londonDateElement.innerHTML = moment().tz("Europe/London").format("MMMM Do YYYY");
+    let londonTime = moment().tz("Europe/London");
+    londonTimeElement.innerHTML = londonTime.format("h:mm:ss [<span class=\"meridiem\">]A[</span>]");
+}
 
-let losAngelesDateElement = losAngelesElement.querySelector(".date");
-let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-
-let londonDateElement = londonElement.querySelector(".date");
-let londonTimeElement = londonElement.querySelector(".time");
-
-losAngelesDateElement.innerHTML = "June 23rd 2026";
-losAngelesTimeElement.innerHTML = `7:40:15 <span class="meridiem">PM</span>`;
-
-londonDateElement.innerHTML = "June 23rd 2026";
-londonTimeElement.innerHTML = `11:40:15 <span class="meridiem">PM</span>`;
+updateTime()
+setInterval(updateTime, 1000);
